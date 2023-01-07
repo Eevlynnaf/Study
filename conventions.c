8a5b01e2336b68395ef5a1a98ccf25415b5df1a8
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-01-07 19:36:15 +0000

    Sat, Jan 7, 2023, 7:36 PM +00:00

--- conventions.c
+++ conventions.c
@@ -3,4 +3,8 @@ since its a convention to use %s as a placeholder, to type % you need to type %%
 == equality
 = assignment
 
+when you're dealing with strings in C,
+you use "" for text
+'' for single characters
+
 */
