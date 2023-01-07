d27632fea14d6e7bd52bd7c2083e2a706d34c98a
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-01-07 19:57:08 +0000

    Sat, Jan 7, 2023, 7:57 PM +00:00

--- loops.c
+++ loops.c
@@ -3,9 +3,17 @@
 
 int main(void)
 
-    int counter = 3;
+    /*int counter = 3;
     while (counter > 0)
     {
         printf("meow\n");
         counter = counter -1;
     }
+    */
+
+   int i = 3;
+   while (i > 0)
+   {
+    printf("meow\n");
+    i--;
+   }
