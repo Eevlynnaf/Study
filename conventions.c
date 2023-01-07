93b4e43f87b4091c9f254e2185a8157d7fb4e89e
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-01-07 19:16:06 +0000

    Sat, Jan 7, 2023, 7:16 PM +00:00

--- conventions.c
+++ conventions.c
@@ -1,1 +1,6 @@
-/* since its a convention to use %s as a placeholder, to type % you need to type %% */
+/*
+since its a convention to use %s as a placeholder, to type % you need to type %%
+== equality
+= assignment
+
+*/
