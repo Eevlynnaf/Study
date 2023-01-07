a696daf7d761e4412c5ab8f9df157c70b8fabc35
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-01-07 19:51:20 +0000

    Sat, Jan 7, 2023, 7:51 PM +00:00

--- conventions.c
+++ conventions.c
@@ -3,6 +3,9 @@ since its a convention to use %s as a placeholder, to type % you need to type %%
 == equality
 = assignment
 || or
+++ adds 1
+-- subtracts 1
+
 
 when you're dealing with strings in C,
 you use "" for text
---
+++ synthaticsugar.c
@@ -0,0 +1,7 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+
+    int counter = 0;
+    counter = counter + 1; /* synthatic sugar: (counter += 1;) if it's always going to be +1 -> (counter++;) */
