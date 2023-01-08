e7e486351091d425bcae0084303f6a23a05a49fb
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-01-08 18:02:00 +0000

    Sun, Jan 8, 2023, 6:02 PM +00:00

--- mario.c
+++ mario.c
@@ -2,9 +2,14 @@
 
 int main(void)
 {
-    for (int i = 0; i < 4; i++);
+    for (int i = 0; i < 4; i++)
     {
         printf("?");
     }
     printf("\n");
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("#\n");
+    }
 }
