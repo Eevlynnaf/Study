d40ab95d512150a04c2d42e2aeaf27f920976af3
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-01-11 17:38:12 +0000

    Wed, Jan 11, 2023, 5:38 PM +00:00

---
+++ mario4.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Get size of grid
+    int n = get_size();
+
+    // Print grid of bricks
+    print_grid(n);
+}
+
+int get_size(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1);
+    return n;
+}
