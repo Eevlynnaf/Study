ec1346a1a5bcc212ece49fb7ef08a2fcd75c39fb
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-01-12 11:05:38 +0000

    Thu, Jan 12, 2023, 11:05 AM +00:00

--- JS/number*.js
+++ JS/number*.js
@@ -2,3 +2,5 @@ console.log(7 / 0)
 console.log("10" / 2)
 console.log("Show!" * 2)
 console.log(0.1 + 0.7)   //impreciso
+//console.log(10.toString())   nao funciona com valor literal
+console.log((10.345).toFixed(2))
