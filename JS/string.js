79e11de51162b2e22c6d4c5686aa9bb41c55638e
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-01-12 11:22:44 +0000

    Thu, Jan 12, 2023, 11:22 AM +00:00

--- JS/string.js
+++ JS/string.js
@@ -5,5 +5,7 @@ console.log(escola.charAt(4))
 console.log(escola.charCodeAt(3)) //retorna valor na tabela ASCII/unicode
 console.log(escola.indexOf('3'))  //retorna indica do digito 3
 
+console.log(escola.substring(1))
+console.log(escola.substring(0, 3))
 
 
