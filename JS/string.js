8283c5641b3a158dc9e28a1220ba3688f8102b96
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-01-12 11:17:20 +0000

    Thu, Jan 12, 2023, 11:17 AM +00:00

--- JS/string.js
+++ JS/string.js
@@ -1,3 +1,7 @@
 const escola = "Cod3r"
 
 console.log(escola.charAt(4))  //retorna caracter no ìndice 4 (r)
+console.log(escola.charAt(4))
+console.log(escola.charCodeAt(3)) //retorna valor na tabela ASCII/unicode
+
+
