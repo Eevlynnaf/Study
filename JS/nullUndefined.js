bfb1a61bd13158d32b727de4e6163965549ddbca
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-01-13 12:40:49 +0000

    Fri, Jan 13, 2023, 12:40 PM +00:00

--- JS/nullUndefined.js
+++ JS/nullUndefined.js
@@ -2,3 +2,7 @@ let valor     //nao inicializada
 console.log(valor)
 
 valor = null  //definida, mas declara ausência de valor
+console.log(valor)
+//console.log(valor.toString())   // Erro, nao se pode acessar uma variavel com valor nulo
+
+const produto = {}
