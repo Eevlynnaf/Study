db8b6416320ec19d3bd90508a67bd8dd04417771
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-01-13 12:20:37 +0000

    Fri, Jan 13, 2023, 12:20 PM +00:00

--- JS/object.js
+++ JS/object.js
@@ -3,6 +3,14 @@
 const prod1 = {}
 prod1.nome = 'Celular'
 prod1.preco = 4998.98
-prod1('Desconto Legal') = 0.40    //evitar atributos com espaco
+prod1['Desconto Legal'] = 0.40    //evitar atributos com espaco
 
 console.log(prod1)
+
+const prod2 =
+{
+    nome: 'Camisa polo',
+    preco: 79.90
+}
+
+console.log(prod2)
