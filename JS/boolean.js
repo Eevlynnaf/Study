6490b6ee1fdb81861853c99710c230f410f85850
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-01-13 11:13:14 +0000

    Fri, Jan 13, 2023, 11:13 AM +00:00

--- JS/boolean.js
+++ JS/boolean.js
@@ -6,3 +6,12 @@ console.log(isAtivo)
 
 isAtivo = 1
 console.log(!!isAtivo)
+
+console.log('Os verdadeiros...')
+console.log(!!3)
+console.log(!!-1)
+console.log(!!' ')
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!(isAtivo = true))
