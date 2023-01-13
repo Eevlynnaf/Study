c4242078d804ce432f90352ae92be44dae033f60
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-01-13 12:18:13 +0000

    Fri, Jan 13, 2023, 12:18 PM +00:00

--- JS/object.js
+++ JS/object.js
@@ -1,3 +1,8 @@
 // json (javascript object notation) é um formato textual, diferente do objeto
 
-const prod1 =
+const prod1 = {}
+prod1.nome = 'Celular'
+prod1.preco = 4998.98
+prod1('Desconto Legal') = 0.40    //evitar atributos com espaco
+
+console.log(prod1)
