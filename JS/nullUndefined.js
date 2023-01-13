79513e6172a1769c01a16a6d75574cd1a96b140d
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-01-13 12:45:07 +0000

    Fri, Jan 13, 2023, 12:45 PM +00:00

--- JS/nullUndefined.js
+++ JS/nullUndefined.js
@@ -6,3 +6,12 @@ console.log(valor)
 //console.log(valor.toString())   // Erro, nao se pode acessar uma variavel com valor nulo
 
 const produto = {}
+console.log(produto.preco)
+console.log(produto)
+
+produto.preco = 3.50
+console.log(produto)
+
+produto.preco = undefined     //evitar atribuir undefined
+console.log(!!produto.preco)
+console.log(produto)
