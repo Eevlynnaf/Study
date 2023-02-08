17600f2c1fc6e35fb92a4085d99db0fffcdcfe59
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-02-08 19:18:51 +0000

    Wed, Feb 8, 2023, 4:18 PM -03:00

--- CS50/C/datatypes.c
+++ CS50/C/datatypes.c
@@ -1,13 +1,55 @@
-/* data types:
-bool
-char
-double
-float
+/*
+Datatypes
 int
-long
-string
-...
+The int data type is used for variables that will store integers
+integers always take up 4 bytes of memory (32bits). This means the range of values they can store is necessarily to 32 bits worth o information
+integer range:
+-2^31 -> 0 -> +2^31 -1
+half for negative numbers and half for positive numbers
 
-these are primitive data types that will be used in CS50
+unsigned int
+unsigned is a qualifier that can be applied to certain types(including int)
+which effectively doubles the positive range of variables of that type
+at the cost of disallowing any negative values
+unsigned int range:
+0 ->  2^32 -1
+
+
+char
+the char data type is used for variables that will store single characters
+characters always take up 1 byte of memory (8bits)
+this means the range of values they can store is necessarily limited to 8 bits worth of information
+thanks to ASCII we've developed a mapping of characters like A, B, C... to numeric values in the positive side of this range
+char range:
+-128 -> 127
+
+float
+the float data type is used for variables that will store floating-point values, also known as real numbers
+floating point values always take up 4 bytes of memory (32bits)
+it's complicated to describe the range of a float, but suffice it to say with 32 bits of precision, some of which might be used
+for an integer part, we are limited in how precise we can be
+
+double
+the double data type is used for variables that will store
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 */
