11b435397ed8b9e107666669f943c914d52c2c54
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-02-08 19:03:06 +0000

    Wed, Feb 8, 2023, 4:03 PM -03:00

--- CS50/C/notebook.c
+++ CS50/C/notebook.c
@@ -1,6 +1,8 @@
 /*
 Datatypes
 int
+The int data type is used for variables that will store integers
+integers always take up 4 bytes of memory (32bits). This means the range of values they can store is necessarily to 32 bits worth o information
 
 
 
