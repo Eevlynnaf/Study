e05c9fc52e6a9a46d81ff6f91022b873c899f9b7
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-02-08 19:08:46 +0000

    Wed, Feb 8, 2023, 4:08 PM -03:00

--- CS50/C/notebook.c
+++ CS50/C/notebook.c
@@ -4,13 +4,15 @@ int
 The int data type is used for variables that will store integers
 integers always take up 4 bytes of memory (32bits). This means the range of values they can store is necessarily to 32 bits worth o information
 integer range:
--2^31 0 +2^31 -1
+-2^31 -> 0 -> +2^31 -1
 half for negative numbers and half for positive numbers
 
 unsigned int
 unsigned is a qualifier that can be applied to certain types(including int)
 which effectively doubles the positive range of variables of that type
 at the cost of disallowing any negative values
+unsigned int range:
+0 ->  2^32 -1
 
 
 
