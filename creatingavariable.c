fff0852838d1d5c7fbef54428e6a685c8fe20cdf
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-02-12 11:43:39 +0000

    Sun, Feb 12, 2023, 8:43 AM -03:00

--- creatingavariable.c
+++ creatingavariable.c
@@ -9,7 +9,19 @@ If you wish to create multiple variables of the same type, you specify the name 
 int height, width;
 float sqrt2, sqrt3, pi;
 
+Using a variable
+After a variable has been declared, it's no longer necessary to specify that variable's type
 
+int number;         //declaration
+number = 17;        //assignment
+char letter;        //declaration
+letter = 'H';       //assignment
+
+If you are simultaneously declaring and setting the value of a variable (sometimes called initializing)
+you can consolidate this to one step
+
+int number = 17;        //initialization
+chat letter = 'H';      //initialization
 
 
 
