87094cc6f365db2c8b36308c400094101321ddbe
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-02-12 11:50:31 +0000

    Sun, Feb 12, 2023, 8:50 AM -03:00

---
+++ CS50/C/Notes/operators.c
@@ -0,0 +1,36 @@
+/*
+
+Arithmetic operators
+
+In C we can add (+), subtract (-), multiply (*) and divide numbers (/), as expected
+
+int x = y + 1;
+x = x * 5;
+
+We also have the modulus operator 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
