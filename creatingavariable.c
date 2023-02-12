9e657c92808b8ee342678b480719c002b531beb5
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-02-12 11:37:53 +0000

    Sun, Feb 12, 2023, 8:37 AM -03:00

---
+++ creatingavariable.c
@@ -0,0 +1,34 @@
+/*
+To bring a variable into existence, you need simply specify the data type of the variable and give it a name
+
+int number;
+char letter;
+
+If you wish to create multiple variables of the same type, you specify the name once, and then list as many variables of that type as you want
+
+int height, width;
+float sqrt2, sqrt3, pi;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
