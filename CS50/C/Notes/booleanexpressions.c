23bd3285ed385070f6541142528761e2c4962c9e
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-02-14 15:31:16 +0000

    Tue, Feb 14, 2023, 12:31 PM -03:00

---
+++ CS50/C/Notes/booleanexpressions.c
@@ -0,0 +1,32 @@
+/*
+Boolean expressions are used in C for comparing values
+All boolean expressions in C evaluate to one of two possible values - true or false
+
+We can see the result of evaluating a Boolean expression in other programming constructs such as deciding which branch in a conditional to take,
+or determining wether a loop should continue to run.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
