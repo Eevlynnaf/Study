bae5ab8fcadf586298972e2163f52c32ba1f8ce2
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-02-14 15:46:56 +0000

    Tue, Feb 14, 2023, 12:46 PM -03:00

---
+++ CS50/C/Notes/conditionalstatements.c
@@ -0,0 +1,29 @@
+/*
+Conditional expresions allow you programs to make decisions and take different forks in the road,
+depending on the values of variables or user input
+
+C provides a few different ways to implement conditional expressions (also known as branches)
+in your programs, some of which likely look familiar from Scratch
+
+if (boolean-expression)
+{
+
+}
+
+If the boolean-expression evaluates to true, all lines of code between the curly braces will execute in order from top-to-bottom
+If the boolean-expression evaluates to false, those lines of code will not execute
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
