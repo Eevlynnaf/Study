4ffdb9337546495f71b3e9e7217ab5c7e7174566
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-02-14 15:33:46 +0000

    Tue, Feb 14, 2023, 12:33 PM -03:00

--- CS50/C/Notes/booleanexpressions.c
+++ CS50/C/Notes/booleanexpressions.c
@@ -4,6 +4,10 @@ All boolean expressions in C evaluate to one of two possible values - true or fa
 
 We can see the result of evaluating a Boolean expression in other programming constructs such as deciding which branch in a conditional to take,
 or determining wether a loop should continue to run.
+Sometimes when working with Boolean expressions we will use variables of type bool, but we don't have to
+In C, every nonzero value is equivalent to true, and zero is false
+
+Two main types of Boolean expressions: logical operators and relational operators
 
 
 
