718e9ba80d54b674594bce6f857c81027ad6eb80
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-02-15 10:03:34 +0000

    Wed, Feb 15, 2023, 7:03 AM -03:00

--- CS50/C/Notes/commandline.c
+++ CS50/C/Notes/commandline.c
@@ -3,7 +3,10 @@ The CS50 IDE (Integrated Development Environment) is a cloud-based machine runni
 Many modern Linux distributions have graphical user interfaces (GUI) to allow easy mouse-based navigation
 Still, as a programmer you'll likely be using your terminal window frequently, and you can do many of the same tasks with keyboard commands
 
+Let's have a look at some of the most important of these keyboard-based commands for working within the IDE of any UNIX-based system
 
+ls
+    Short for list, this command will give you a readout of all the files and folders in your current directory
 
 
 
