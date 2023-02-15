e5fa5cb15f37e466f501cfb09d2103a212c77c4c
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-02-15 09:52:54 +0000

    Wed, Feb 15, 2023, 6:52 AM -03:00

--- CS50/C/Notes/loops.c
+++ CS50/C/Notes/loops.c
@@ -17,8 +17,25 @@ while (boolean-expression)
 If the boolean-expr evaluates to true, all lines of code between the curly braces will execute repeatedly, in order from top-to-bottom, until boolean-expr evaluates to false
 Somewhat confusingly, the behavior of the Scratch block is reversed, but it's the closest analog
 
+do
+{
 
+}
+while (boolean-expr);
+This loop will execute all lines of code between the curly braces once, and then, if the boolean-expr evaluates to true, will go back and repeat that process until
+boolean-expr evaluates to false
 
+for (int i = 0; i < 10; i++)
+{
+
+}
+Syntactically unattractive, but for loops are used to repeat the body of a loop a specified number of times, in this example 10.
+The process undertaken in a for loop is:
+    The counter variable(s) (here, i) is set
+    The boolean expression is checked
+        If it evaluates to true, the body of the loop executes
+        If it evaluates to false, the body of the loop does not execute
+    The counter variable is incremented, and then the Boolean expression is checked again
 
 
 
