37fd80b76bc32557cc03b7922a9651fa89237ee6
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-02-15 09:44:00 +0000

    Wed, Feb 15, 2023, 6:44 AM -03:00

--- CS50/C/Notes/loops.c
+++ CS50/C/Notes/loops.c
@@ -1,6 +1,14 @@
 /*
+Loops allow your programs to execute lines of code repeatedly, saving you from needing to copy and paste or otherwise repeat lines of code
+C provides a few different ways to implement loops in your programs, some of which likely look familiar from Scratch
 
+while (true)
+{
 
+}
+This is what we call an infinite loop
+The lines of code between the curly braces will execute repeatedly from top-to-bottom, until and unless we break out of it(as with a break; statement)
+or otherwise kill our program
 
 
 
