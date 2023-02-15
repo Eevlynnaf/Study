0209daf2ae0e83a8210f9f782db96d8a2aa65f8d
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-02-15 10:01:55 +0000

    Wed, Feb 15, 2023, 7:01 AM -03:00

---
+++ CS50/C/Notes/commandline.c
@@ -0,0 +1,37 @@
+/*
+The CS50 IDE (Integrated Development Environment) is a cloud-based machine running Ubuntu, one of the many flavors of the Linux OS
+Many modern Linux distributions have graphical user interfaces (GUI) to allow easy mouse-based navigation
+Still, as a programmer you'll likely be using your terminal window frequently, and you can do many of the same tasks with keyboard commands
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
