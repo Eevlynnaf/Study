3ffb7df53c5c20984b33a7ff93061c69ad621c66
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-02-16 11:20:18 +0000

    Thu, Feb 16, 2023, 8:20 AM -03:00

--- CS50/projects/problemsets/problemset1/mario-more/mario.c
+++ CS50/projects/problemsets/problemset1/mario-more/mario.c
@@ -8,4 +8,13 @@ int main(void)
         int h = get_int("Height: ");
     }
     while (h < 0; h > 8)
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <n; j++)
+        {
+            printf()
+        }
+    }
+    printf("\n");
 }
