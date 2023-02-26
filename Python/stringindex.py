9f6d6f2928f5fbe316b13d3615c0c3c53354febc
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-02-26 19:42:52 +0000

    Sun, Feb 26, 2023, 4:42 PM -03:00

--- Python/stringindex.py
+++ Python/stringindex.py
@@ -4,7 +4,9 @@ selfish = "me me me"
 print(selfish[0])
 print(selfish[1])
 
-# [start:stop:stepover]
+# [start:stop:stepover]  // string slicing
 print(selfish[3:5])
 print(selfish[0:8])
 print(selfish[0:8:2])
+print(selfish[-1])
+print(selfish[::-1])
