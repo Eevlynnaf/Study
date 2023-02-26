b9a114f016fc9b4e6aa60ce0fc5453e6623bd4a7
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-02-26 19:30:29 +0000

    Sun, Feb 26, 2023, 4:30 PM -03:00

--- Python/formattedstrings.py
+++ Python/formattedstrings.py
@@ -2,4 +2,6 @@
 name = "Johnny"
 age = 55
 
-print(f"hi {name}. You are {age} years old")
+print(f"hi {name}. You are {age} years old.")
+
+print("hi {}. You are {} years old.".format("Johnny", "55"))
