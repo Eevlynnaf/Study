47b141cc01b81b13296829bdb52672001a32c5a8
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-02-27 10:30:11 +0000

    Mon, Feb 27, 2023, 7:30 AM -03:00

--- Python/birthyear.py
+++ Python/birthyear.py
@@ -1,4 +1,8 @@
-birth_year = input("What year were you born?")
-while (birth_year > 1900 | birth_year < 2023)
-
+birth_year =
 years_old = (2023 - birth_year)
+
+while (birth_year > 1900 | birth_year < 2023)
+{
+    birth_year = input("What year were you born?")
+}
+print(f"You are {years_old}!)
