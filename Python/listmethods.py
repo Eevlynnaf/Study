4ad7533d82ea91e65ae19eea9a10c71d0a0ad8d8
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-02-28 10:48:17 +0000

    Tue, Feb 28, 2023, 7:48 AM -03:00

--- Python/listmethods.py
+++ Python/listmethods.py
@@ -1,6 +1,7 @@
 basket = [1,2,3,4,5]
 
 new_list = basket.append(100)
+new_list = basket.insert(2, 300)
 print(basket)
 print(new_list)      #.append will add to the existing variable but will not give new results inside a new variable
-print(basket.insert(2, 300))
+
