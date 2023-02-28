f0e895e165ec00a98ea98a79542d8d0b5122bbf2
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-02-28 10:37:15 +0000

    Tue, Feb 28, 2023, 7:37 AM -03:00

--- Python/lists.py
+++ Python/lists.py
@@ -1,1 +1,14 @@
 #List slicing
+string = 'helloooo'
+print(string[0:2:1])
+
+amazon_cart = [
+    'notebooks',
+    'sunglasses'
+    'toys'
+    'grapes'
+    ]
+
+amazon_cart[0] = 'laptop'
+print(amazon_cart[0::2])
+print(amazon_cart)
