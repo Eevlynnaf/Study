1387db9a6000f0e4aa149fa60000a23760c581d5
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-03-01 12:27:07 +0000

    Wed, Mar 1, 2023, 9:27 AM -03:00

--- Python/sets.py
+++ Python/sets.py
@@ -5,3 +5,14 @@
 my_set = {1,2,3,4,5,5}
 my_set.add(100)
 print(my_set)
+print(5 in my_set)
+print(len(my_set))
+
+print(list(my_set))     #converting the set into a list
+
+new_set = my_set.copy()
+
+#print(my_set[0])       #sets dont support indexing
+
+my_list = [6,7,8,9,10]      #converting a list to a set
+print(set(my_list))
