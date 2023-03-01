d2cf267aba928a817182d959a13f9aa63465affd
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-03-01 12:20:21 +0000

    Wed, Mar 1, 2023, 9:20 AM -03:00

--- Python/tuples.py
+++ Python/tuples.py
@@ -5,4 +5,13 @@ my_tuple = (1,2,3,4,5)
 print(my_tuple[1])
 print(5 in my_tuple)
 
+print(my_tuple.count(4))
+print(my_tuple.index(3))
+print(len(my_tuple))
+
 new_tuple = my_tuple[1:2]
+print(new_tuple)
+
+x,y,z, *other = (5,6,7,8,9)
+print(z)
+print(other)
