b56478c13f8fe27f8341faf4d567e26e30ce2ca4
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-03-01 12:23:15 +0000

    Wed, Mar 1, 2023, 9:23 AM -03:00

---
+++ Python/sets.py
@@ -0,0 +1,7 @@
+#sets are unordered collections of unique objects
+#it doesnt return duplicates
+#the elements of the set are in different places on memory
+
+my_set = {1,2,3,4,5,5}
+my_set.add(100)
+print(my_set)
