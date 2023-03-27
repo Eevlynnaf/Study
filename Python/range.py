b9ef044972ee65ea5e63af23b0b0419d7d2eb8e5
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-03-27 06:13:14 +0000

    Mon, Mar 27, 2023, 3:13 AM -03:00

--- Python/range.py
+++ Python/range.py
@@ -1,3 +1,5 @@
+#used most of the time in for loops, to have some action take place a certain amount of times
+
 print(range(100))
 
 for number in range(0, 100):
@@ -5,3 +7,6 @@ for number in range(0, 100):
 
 for _ in range(0, 10, 2):
     print(_)
+
+for i in range(10, 0, -1):
+    print(i)
