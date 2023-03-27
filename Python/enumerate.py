d4b83ef431271d6b3fa15e153494adeda3488fa6
Evelyn Afonso <112144281+Eevlynnaf@users.noreply.github.com>
2023-03-27 06:20:50 +0000

    Mon, Mar 27, 2023, 3:20 AM -03:00

--- Python/enumerate.py
+++ Python/enumerate.py
@@ -5,3 +5,8 @@ for i, char in enumerate('Hellooo'):
 
 for i, char in enumerate([1,2,3,4]):
     print(i, char)
+
+
+for i, char in enumerate(list(range(100))):
+    if char == 50:
+        print(f'index of 50 is: {i}')
